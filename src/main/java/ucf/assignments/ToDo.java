57d092e79895fcc99a1a7909df931e1f84bee618
polvnco <cpolanco000@gmail.com>
2021-07-03 16:27:12 -0400

    realized previous issues and resolved them. still need to complete the psuedo stuff and puml but generally the GUI is what I'd like it to be. psuedo should be done tomorrrow.

--- src/main/java/ucf/assignments/ToDo.java
+++ src/main/java/ucf/assignments/ToDo.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 
 public class ToDo extends Application {
 
+
     @Override
     public void start(Stage stage) {
         try {
--- src/main/java/ucf/assignments/ToDoController.java
+++ src/main/java/ucf/assignments/ToDoController.java
@@ -32,7 +32,20 @@ public class ToDoController {
         stage.setScene(scene);
         stage.show();
     }
+    public void switchToSettingsScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("Settings.fxml"))));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-
+    public void switchToAboutScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("About.fxml"))));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
