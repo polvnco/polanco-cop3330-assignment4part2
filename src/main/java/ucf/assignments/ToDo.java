7280ed002203e3ea134434b54c68b85d142752d4
polvnco <cpolanco000@gmail.com>
2021-07-02 21:43:32 -0400

    scene stuff

--- src/main/java/ucf/assignments/ToDo.java
+++ src/main/java/ucf/assignments/ToDo.java
@@ -1,4 +1,30 @@
 package ucf.assignments;
 
-public class ToDo {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ToDo extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("ToDo.fxml"))));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
