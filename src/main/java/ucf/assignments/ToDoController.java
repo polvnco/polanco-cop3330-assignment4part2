ffc59fd63446a890eabd68ead347615afdd0e21f
polvnco <cpolanco000@gmail.com>
2021-07-02 21:43:53 -0400

    scene controlla

---
+++ src/main/java/ucf/assignments/ToDoController.java
@@ -0,0 +1,38 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ToDoController {
+    public Pane pane;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToToDoScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("ToDo.fxml"))));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToAllListScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("AllList.fxml"))));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+}
